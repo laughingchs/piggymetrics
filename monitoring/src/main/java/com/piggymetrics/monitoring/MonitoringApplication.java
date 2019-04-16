fa81c10d02214a66e38ba28f605ac5ba1759dd53
谷月 <husheng.chenhs@alipay.com>
2019-04-16 21:44:43 +0800

    修复Hystrix Dashboard展示问题

--- monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
+++ monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
@@ -1,14 +1,42 @@
 package com.piggymetrics.monitoring;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableHystrixDashboard
 public class MonitoringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MonitoringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringApplication.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+
+        ServletRegistrationBean regBean = new ServletRegistrationBean(streamServlet);
+
+        regBean.setLoadOnStartup(1);
+
+        List mappingList = new ArrayList();
+
+        mappingList.add("/hystrix.stream");
+
+        regBean.setUrlMappings(mappingList);
+
+        regBean.setName("hystrixServlet");
+
+        return regBean;
+
+    }
+
 }
